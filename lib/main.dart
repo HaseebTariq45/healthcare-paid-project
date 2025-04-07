4bcf95c71713ef59b473306b0dfc763be48ee263
HaseebTariq45 <haseebawang4545@gmail.com>
2025-04-07 23:02:48 +0500

    FireBase_Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:healthcare/views/screens/onboarding/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:healthcare/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
